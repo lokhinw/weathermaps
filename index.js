e2c9feaa13e33d34c1a5667997c7215aceb1ea96
Lokhin Wu <lokhin.wu@live.com>
2016-12-03 11:21:57 -0500

    32prk2

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 var Forecast = require('forecast');
 var moment = require('moment');
+var latitude = 43.6425585;
+var longitude = -79.387092;
+var forecast = new Forecast({
+    service: 'darksky',
+    key: '6dc9ae2624533f631c08674cd447483b',
+    units: 'celcius',
+    cache: true, // Cache API requests
+    ttl: { // How long to cache requests. Uses syntax from moment.js: http://momentjs.com/docs/#/durations/creating/
+        minutes: 27,
+        seconds: 45
+    }
+});
 
-// var forecast = new Forecast({
-//     service: 'darksky',
-//     key: '6dc9ae2624533f631c08674cd447483b',
-//     units: 'celcius',
-//     cache: true, // Cache API requests
-//     ttl: { // How long to cache requests. Uses syntax from moment.js: http://momentjs.com/docs/#/durations/creating/
-//         minutes: 27,
-//         seconds: 45
-//     }
-// });
-//
-// // Retrieve weather information from coordinates (Sydney, Australia)
-// forecast.get([43.6425585,-79.387092], function(err, weather) {
-//     if (err) return console.dir(err);
-//     console.log(JSON.stringify(weather, null, 2));
-// });
+// Retrieve weather information from coordinates (Sydney, Australia)
+forecast.get([latitude,longitude], function(err, weather) {
+    if (err) return console.dir(err);
 
-console.log(moment.unix(1480779935).format("hh"));
+    var currentTime = (weather.daily.data[0].time);
+    console.log(currentTime);
+    // console.log(moment.unix(currentTime).format("hh"));
+});
