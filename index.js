83cbbb48e3eba320150c18adbf58c936e7bc16e7
Lokhin Wu <lokhin.wu@live.com>
2016-12-03 13:03:30 -0500

    forecast

--- index.js
+++ index.js
@@ -1,28 +1,45 @@
 let Forecast = require('forecast');
 let moment = require('moment');
-let latitude = 43.6425585;
-let longitude = -79.387092;
+// let latitude = 55.748517; //43.6425585;toronto
+// let longitude = 37.0720873; //-79.387092;toronto
 
-module.exports = function(){
-  var forecast = new Forecast({
-      service: 'darksky',
-      key: '6dc9ae2624533f631c08674cd447483b',
-      units: 'celcius',
-      cache: true, // Cache API requests
-      ttl: { // How long to cache requests. Uses syntax from moment.js: http://momentjs.com/docs/#/durations/creating/
-          minutes: 27,
-          seconds: 45
-      }
-  });
-
-  return function(){
-    // Retrieve weather information from coordinates (Sydney, Australia)
-    forecast.get([latitude,longitude], function(err, weather) {
-        if (err) return console.dir(err);
-
-        let currentTime = (weather.daily.data[0].time);
-        console.log(currentTime);
-        // console.log(moment.unix(currentTime).format("hh"));
+module.exports = function() {
+    var forecast = new Forecast({
+        service: 'darksky',
+        key: '6dc9ae2624533f631c08674cd447483b',
+        units: 'celcius',
+        cache: true, // Cache API requests
+        ttl: { // How long to cache requests. Uses syntax from moment.js: http://momentjs.com/docs/#/durations/creating/
+            minutes: 27,
+            seconds: 45
+        }
     });
-  };
+
+    return function(latitude, longitude, hrsFromNow) {
+        // Retrieve weather information from coordinates (Sydney, Australia)
+        forecast.get([latitude, longitude], function(err, weather) {
+            if (err) return console.dir(err);
+            //  console.dir(weather);
+            // for (let i = 0; i < weather.hourly.data.length; i++) {
+            //     console.log(weather.hourly.data[i].time);
+            // }
+            //
+            // let currentTime = (weather.daily.data[0].time);
+            // let currentTimeHR = console.log(moment.unix(currentTime).format("hh"));
+            // let currentWeather = (weather.daily[0].temperature);
+            //  let futureWeather = ();
+            // console.log(currentTimeHR);
+            // return currentTimeHR;
+            // return currentWeather;
+            //
+            if (hrsFromNow < 49) {
+                return {
+                    temperature: weather.hourly.data[hrsFromNow].temperature,
+                    precipType: weather.hourly.data[hrsFromNow].precipType
+                };
+            } else {
+                return {};
+            }
+        });
+    };
 };
--- main.js
+++ main.js
@@ -3,18 +3,23 @@ let body_parser = require("body-parser");
 let maps_api = require("./scripts/maps.js")();
 let forecast = require("./index.js")();
 let app = express();
-let listener = app.listen(8080, 'localhost', function () {
+let listener = app.listen(8080, 'localhost', function() {
     console.log("Server is listening on port 8080")
 });
-
+var latitude = 49;
+var longitude = -73;
+var hrsFromNow = 30;
 app
     .use(body_parser.json())
-    .post("/api/maps/distance_mat", function (req, res, next) {
+    .post("/api/maps/distance_mat", function(req, res, next) {
         let c = req.body;
-        maps_api.distance_matrix([-79,-79], [-80,-80], new Date().getTime(), function(res){
-           console.log(JSON.stringify(res));
+        maps_api.distance_matrix([-79, -79], [-80, -80], new Date().getTime(), function(res) {
+            console.log(JSON.stringify(res));
             res.end(res);
         });
 
     })
     .use(express.static('static'));
+if (hrsFromNow < 49) {
+    console.log(forecast(latitude, longitude, hrsFromNow).temperature);
+}
