01df66d8bdf40a9356a5aa2938b5f28a36817139
Lokhin Wu <lokhin.wu@live.com>
2016-12-03 10:38:06 -0500

    removed city list

---
+++ index.js
@@ -0,0 +1,21 @@
+var Forecast = require('forecast');
+var moment = require('moment');
+// Initialize
+// var forecast = new Forecast({
+//     service: 'darksky',
+//     key: '6dc9ae2624533f631c08674cd447483b',
+//     units: 'celcius',
+//     cache: true, // Cache API requests
+//     ttl: { // How long to cache requests. Uses syntax from moment.js: http://momentjs.com/docs/#/durations/creating/
+//         minutes: 27,
+//         seconds: 45
+//     }
+// });
+//
+// // Retrieve weather information from coordinates (Sydney, Australia)
+// forecast.get([43.6425585,-79.387092], function(err, weather) {
+//     if (err) return console.dir(err);
+//     console.log(JSON.stringify(weather, null, 2));
+// });
+
+console.log(moment().valueOf(1480779247));
